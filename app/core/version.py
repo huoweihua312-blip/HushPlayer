b6299d4565b551a7ec8fb9da869d7a676a6fc90b
Niko <17351610+milcream@user.noreply.gitee.com>
2026-08-15 14:09:24 +0800

    release: publish HushPlayer 0.6.0-beta.8

--- app/core/version.py
+++ app/core/version.py
@@ -5,8 +5,8 @@ from collections.abc import Sequence
 
 
 APP_NAME = "HushPlayer"
-APP_VERSION = "0.6.0-beta.7"
-APP_NUMERIC_VERSION = (0, 6, 0, 7)
+APP_VERSION = "0.6.0-beta.8"
+APP_NUMERIC_VERSION = (0, 6, 0, 8)
 UPDATE_CHANNEL = "beta"
 UPDATE_ARCHITECTURE = "win-x64"
 UPDATE_MANIFEST_SOURCES = (
