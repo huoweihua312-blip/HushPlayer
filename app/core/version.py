db4db224c6231dc3b43d0a317d9b7b530f5e42fa
Niko <17351610+milcream@user.noreply.gitee.com>
2026-09-03 11:32:33 +0800

    chore(release): prepare HushPlayer 0.6.0-beta.13

--- app/core/version.py
+++ app/core/version.py
@@ -5,8 +5,8 @@ from collections.abc import Sequence
 
 
 APP_NAME = "HushPlayer"
-APP_VERSION = "0.6.0-beta.12"
-APP_NUMERIC_VERSION = (0, 6, 0, 12)
+APP_VERSION = "0.6.0-beta.13"
+APP_NUMERIC_VERSION = (0, 6, 0, 13)
 UPDATE_CHANNEL = "beta"
 UPDATE_ARCHITECTURE = "win-x64"
 UPDATE_MANIFEST_SOURCES = (
